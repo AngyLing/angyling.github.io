526a170aebe7de3c8ae73759212f9a2ee003d501
AngyLing <86360418+AngyLing@users.noreply.github.com>
2023-01-06 08:15:06 +0300

    first commit

---
+++ views.py
@@ -0,0 +1,8 @@
+from flask import (
+            Flask,
+            flash,  # система сообщений
+            redirect,
+            render_template,
+            request,
+            session,
+            url_for)
